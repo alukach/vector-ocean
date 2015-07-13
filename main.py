e9b50de4577f99903299d94bc2caade8261c388e
Anthony Lukach <anthonylukach@gmail.com>
2015-07-12 23:49:35 -0600

    Update pythin script (although likely not the route taken in future)

--- main.py
+++ main.py
@@ -14,32 +14,17 @@ from numpy import uint8, int16
 
 
 def hillshade(array, azimuth, angle_altitude):
-
+    """ http://geoexamples.blogspot.ca/2014/03/shaded-relief-images-using-gdal-python.html """
     x, y = gradient(array)
     slope = pi/2. - arctan(sqrt(x*x + y*y))
     aspect = arctan2(-x, y)
     azimuthrad = azimuth*pi / 180.
     altituderad = angle_altitude*pi / 180.
 
-
     shaded = sin(altituderad) * sin(slope)\
      + cos(altituderad) * cos(slope)\
      * cos(azimuthrad - aspect)
     return (255*(shaded + 1)/2).astype(int16)
-
-
-def main(infile, outfile):
-    with rasterio.drivers():
-        with rasterio.open(infile) as src:
-
-            meta = src.meta
-            del meta['transform']
-            meta.update(driver='GTiff')
-
-            with rasterio.open(outfile, 'w', **meta) as dst:
-                arr = src.read(1)
-                arr = hillshade(arr, 315, 0.01)
-                dst.write_band(1, arr)
 
 
 def main_threaded(infile, outfile):
@@ -57,7 +42,7 @@ def main_threaded(infile, outfile):
                 def copy_window(window):
                     # Write the result.
                     for i, arr in enumerate(src.read(window=window), 1):
-                        arr = hillshade(arr, 315, 0.01)
+                        arr = hillshade(arr, 315, 45)
                         dst.write_band(i, arr, window=window)
 
                 # Queue up the loop's tasks.
@@ -90,7 +75,4 @@ if __name__ == '__main__':
         help="Run with a pool of worker threads")
     args = parser.parse_args()
 
-    if args.async:
-        main_threaded(args.input, args.output)
-    else:
-        main(args.input, args.output)
+    main_threaded(args.input, args.output)
