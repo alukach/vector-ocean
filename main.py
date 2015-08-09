c8317cf020c6206cd9ad5d19d5645e3ad004e652
Anthony Lukach <anthonylukach@gmail.com>
2015-08-08 18:07:58 -0600

    Add clear-table, db-name, use kwargs, python3 style

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ import tempfile
 import rasterio
 
 
-devnull = open(os.devnull, 'w')
-
-
-def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=None, zoom=8, vert_exag=20, thresholds=(30, 50, 70, 80),
-    verbosity=0, pause=False):
+def task(file_path, db_name, table_name,
+        col, row, src_width, src_height, num_rows,
+        clipfile_path, vert_exag, thresholds,
+        verbosity, pause):
     """
     General steps:
     - Subset file
@@ -21,11 +20,8 @@ def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=N
     - Polygonize
     """
 
-    db_name = 'ocean-tiles'
-
     with tempfile.TemporaryDirectory() as tmpdir:
         overlap = float(2)
-        num_rows = math.pow(2, zoom)
         width = overlap * src_width / num_rows
         height = overlap * src_height / num_rows
         # TODO: If x or y is negative, handle getting selection from other
@@ -39,7 +35,7 @@ def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=N
         cmd = cmd.format(x=x, y=y, width=width, height=height, input=file_path, output=subset_path)
         if verbosity > 1:
             print(cmd)
-        subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+        subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Crop data
         if clipfile_path:
@@ -51,7 +47,7 @@ def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=N
             )
             if verbosity > 1:
                 print(cmd)
-            subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+            subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             clipped_subset_path = "{tmpdir}/subset_clipped.tif".format(tmpdir=tmpdir)
             cmd = "gdalwarp -cutline {clipfile_subset_path} {input} {output}"
             cmd = cmd.format(
@@ -60,7 +56,7 @@ def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=N
             )
             if verbosity > 1:
                 print(cmd)
-            subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+            subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             subset_path = clipped_subset_path
 
         # Hillshade data
@@ -69,7 +65,7 @@ def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=N
         cmd = cmd.format(vert=vert_exag, input=subset_path, output=hillshade_path)
         if verbosity > 1:
             print(cmd)
-        subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+        subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Thresholds
         threshold_base = "{}_threshold".format(hillshade_path)
@@ -80,7 +76,7 @@ def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=N
             cmd = cmd.format(input=hillshade_path, output=threshold_path, threshold=threshold)
             if verbosity > 1:
                 print(cmd)
-            subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+            subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         threshold_paths = " ".join([threshold_path_tmplt.format(threshold=threshold) for threshold in thresholds])
 
         # Combine thresholds
@@ -89,20 +85,20 @@ def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=N
         cmd = cmd.format(threshold_paths=threshold_paths, output=combined_path)
         if verbosity > 1:
             print(cmd)
-        subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+        subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Convert
         combined_tif_path = "{}.tif".format('.'.join(combined_path.split('.')[:-1]))
         cmd = "convert {input} {output}".format(input=combined_path, output=combined_tif_path)
         if verbosity > 1:
             print(cmd)
-        subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+        subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Re-apply geodata
         if verbosity > 1:
             print(cmd)
         cmd = "listgeo -tfw {}".format(subset_path)
-        subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+        subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         cmd = "mv {input}.tfw {output}.tfw"
         cmd = cmd.format(
             input='.'.join(subset_path.split('.')[:-1]),
@@ -110,43 +106,55 @@ def task(file_path, output_dir, col, row, src_width, src_height, clipfile_path=N
         )
         if verbosity > 1:
             print(cmd)
-        subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+        subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Polygonize
         geojson_path = "{}.geojson".format(combined_tif_path)
-        cmd = "gdal_polygonize.py {input} -f PostgreSQL PG:dbname={db_name} {layer_name}"
-        cmd = cmd.format(input=combined_tif_path, db_name=db_name, layer_name=zoom)
+        cmd = "gdal_polygonize.py {input} -f PostgreSQL PG:dbname={db_name} {table_name} value"
+        cmd = cmd.format(input=combined_tif_path, db_name=db_name, table_name=table_name)
         if verbosity > 1:
             print(cmd)
-        subprocess.check_call(cmd, shell=True, stdout=devnull, stderr=devnull)
+        subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         if pause:
-            input(tmpdir + '\n')
-        if verbosity:
-            print('.', end='\n' if verbosity > 1 else '.')
+            input(tmpdir)
+        elif verbosity:
+            print('.', end='\n' if verbosity > 1 else '')
             sys.stdout.flush()
 
 
-def scheduler(file_path, output_dir, verbosity, clipfile_path=None, pause=False):
+def scheduler(**kwargs):
     # TODO: Use kwargs
     with rasterio.drivers():
-        with rasterio.open(file_path) as src:
+        with rasterio.open(kwargs['file_path']) as src:
             width = src.width
             height = src.height
 
     zoom = 8
     for z in range(zoom, zoom+1):
         num_rows = int(math.pow(2, z))
+        table_name = z
+
+        if kwargs['clear_tables']:
+            cmd = 'psql {db_name} -c "DROP TABLE \\"{table_name}\\""'
+            cmd = cmd.format(db_name=kwargs['db_name'], table_name=table_name)
+            subprocess.check_call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
         for col in range(0, num_rows):
             for row in range(0, num_rows):
-                task(
-                    file_path=file_path, output_dir=output_dir,
-                    clipfile_path=clipfile_path,
-                    col=col, row=row, zoom=z,
-                    src_width=width, src_height=height,
-                    vert_exag=20, thresholds=(30, 50, 70, 80),
-                    verbosity=verbosity, pause=pause
-                )
+                task_kwargs = kwargs.copy()
+                del task_kwargs['clear_tables']
+                task_kwargs.update({
+                    'table_name': table_name,
+                    'num_rows': num_rows,
+                    'src_width': width,
+                    'src_height': height,
+                    'col': col,
+                    'row': row,
+                    'vert_exag': 20,
+                    'thresholds': (30, 50, 70, 80),
+                })
+                task(**task_kwargs)
 
 
 if __name__ == '__main__':
@@ -156,40 +164,40 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Concurrent raster processing demo")
     parser.add_argument(
-        'input',
+        'file_path',
         metavar='INPUT',
         help="Input file name")
     parser.add_argument(
         '--clipfile',
-        metavar='CLIPFILE',
+        dest='clipfile_path',
+        metavar='CLIPFILE_PATH',
         help="Clipping Shapefile")
     parser.add_argument(
         '--verbose', '-v',
+        dest='verbosity',
         default=0,
-        action='count'
-    )
+        action='count')
     parser.add_argument(
         '--pause', '-p',
         action='store_true',
         default=False,
-        help=('Pause after each tile is generated, before temporary '
-            'directory is removed. For development/testing purporses.')
-    )
+        help=('Wait for input after each tile is generated, before '
+            'temporary directory is removed. For development/testing '
+            'purposes.') )
     parser.add_argument(
-        '--outdir', '-o',
-        metavar='OUTPUT_DIR',
-        default='./out',
-        help="Output Directory")
-
+        '--db_name', '-db',
+        dest='db_name',
+        metavar='DB_NAME',
+        default='ocean-tiles',
+        help="Output Database")
+    parser.add_argument(
+        '--clear-tables',
+        action='store_true',
+        default=False,
+        help="Clear destination tables before creating tiles")
     args = parser.parse_args()
     try:
-        scheduler(
-            file_path=args.input,
-            output_dir=args.outdir,
-            verbosity=args.verbose,
-            clipfile_path=args.clipfile,
-            pause=args.pause,
-        )
+        scheduler(**args.__dict__)
     except KeyboardInterrupt:
         print("Received exit signal, shutting down...")
         sys.exit()
