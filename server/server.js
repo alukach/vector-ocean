517180a1955f2318811f1e859228afc7348be423
Anthony Lukach <anthonylukach@gmail.com>
2015-09-19 14:03:32 -0600

    Cache tiles to disk

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var http = require('http');
-var app = express();
 var tilelive = require('tilelive');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var argv = require('minimist')(process.argv.slice(2));
+
 require('mbtiles').registerProtocols(tilelive);
 require('tilelive-tmsource')(tilelive);
 
+var app = express();
 tilelive.load('tmsource://./mapbox_studio.tm2source', function(err, source) {
 
     if (err) {
@@ -18,22 +23,47 @@ tilelive.load('tmsource://./mapbox_studio.tm2source', function(err, source) {
         next();
     });
 
-    app.get(/^\/(\d+)\/(\d+)\/(\d+).pbf$/, function(req, res){
+    app.get(/^\/tiles\/(\d+)\/(\d+)\/(\d+).pbf$/, function(req, res){
 
         var z = req.params[0];
         var x = req.params[1];
         var y = req.params[2];
 
-        console.log('get tile %d, %d, %d', z, x, y);
+        var cache_path = path.join(argv.cachedir, "{z}/{x}/{y}.pbf");
+        cache_path = cache_path.replace("{z}", z).replace("{x}", x).replace("{y}", y);
 
-        source.getTile(z, x, y, function(err, tile, headers) {
-            if (err) {
-                res.status(404)
-                res.send(err.message);
-                console.log(err.message);
+        // Look for file in cache
+        fs.stat(cache_path, function(err, stat) {
+            // Serve from cache
+            if (stat) {
+                console.log('get tile %d, %d, %d from cache', z, x, y);
+                res.header('Content-Type', 'application/x-protobuf');
+                res.header('Content-Encoding', 'gzip');
+                fs.createReadStream(cache_path).pipe(res);
+            // Not in cache, get from DB
             } else {
-              res.set(headers);
-              res.send(tile);
+                console.log('get tile %d, %d, %d from db', z, x, y);
+                source.getTile(z, x, y, function(err, tile, headers) {
+                    if (err) {
+                        res.status(404);
+                        res.send(err.message);
+                        console.log(err.message);
+                    } else {
+                        res.set(headers);
+                        res.send(tile);
+
+                        // Cache to disk
+                        if (argv.cachedir) {
+                            mkdirp(path.dirname(cache_path), function (err) {
+                                if (err) return console.error(err);
+                                fs.writeFile(cache_path, tile, function(err) {
+                                    if(err) return console.log(err);
+                                    console.log('wrote %d, %d, %d to cache', z, x, y);
+                                });
+                            });
+                        }
+                    }
+                });
             }
         });
     });
