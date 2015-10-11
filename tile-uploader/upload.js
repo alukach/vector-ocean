bcabe219b491349d3d85cdbf27e47e028556b5df
Anthony Lukach <anthonylukach@gmail.com>
2015-10-11 01:13:47 -0600

    Wrap-up uploader tooling

--- tile-uploader/upload.js
+++ tile-uploader/upload.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var util = require('util');
 var os = require('os');
 var path = require('path');
-var spawn = require('child_process').spawnSync;
+var exec = require('child_process').exec;
 
 var tilelive = require('tilelive');
 var argv = require('minimist')(process.argv.slice(2));
@@ -12,73 +12,87 @@ var mkdirp = require('mkdirp');
 require('mbtiles').registerProtocols(tilelive);
 require('tilelive-tmsource')(tilelive);
 
+var program = require('commander');
 
-var minzoom = 1;
-var maxzoom = 2;
-var bucket = 'ocean2';
+program
+	.version(0.1)
+    .option('-z, --minimum-zoom <zoom>', 'Minimum zoom level to process (defaults to 0).', parseInt)
+    .option('-Z, --maximum-zoom <zoom>', 'Maximum zoom level to process (defaults to 6).', parseInt)
+    .option('-b, --bucket <name>', 'Bucket in which to upload results (defaults to \'bathy\').')
+	.option('-c, --concurrency <count>', 'Number of concurrent tile operations (defaults to 2).', parseInt)
+	.parse(process.argv);
+
+var minzoom = program.minimumZoom || 0;
+var maxzoom = program.maximumZoom || 6;
+var bucket = program.bucket || 'bathy';
+var concurrency = program.concurrency || 2;
 
 tilelive.load('tmsource://../tile-server/mapbox_studio.tm2source', function(err, source) {
     // Queue Handler
-    var q = async.queue(function (task, callback) {
-        var url = util.format('%s/%s/%s', task.z, task.x, task.y);
-        console.log('Procesessing ' + url);
-        source.getTile(task.z, task.x, task.y, function(err, tile, headers) {
-            if (err) {
-                console.log(url, ' failed!');
-            } else {
-                // Save to tmpfile
-                var fname = url + '.pbf';
-                var fdir = os.tmpdir();
-                var fpath = path.join(fdir, fname);
-                console.log(fpath);
-                mkdirp(path.dirname(fpath), function (err) {
-                    if (err) throw (err);
-
-                    fs.writeFile(fpath, tile, function (err) {
-                        if (err) throw (err);
-
-                        // Upload to Swift
-                        var headers = " -H 'Content-Type: application/x-protobuf' -H 'X-Container-Meta-Access-Control-Allow-Origin: *'";
-                        results = spawn('swift', ['upload', bucket, fname], {cwd: path.dirname(fpath)});
-                        if (results.error) throw (results.error);
-                        // console.log("Wrote ", results.output);
-                        console.log("Stdout: ", results.stdout.toString());
-                        console.log("Stderr: ", results.stderr.toString());
-                        console.log("Status: ", results.status);
-                        console.log("Error: ", results.error);
-                    });
-                });
-                callback();
-            }
-        });
-    }, 2);
+    var q = async.queue(tileUploaderTask, 2);
 
     // Finishing Message
     q.drain = function() {
+        var cmd = util.format('swift post %s -H \'X-Container-Meta-Access-Control-Allow-Origin: *\'', bucket);
+        runCmd(cmd);
         console.log('all items have been processed');
         return process.exit();
     };
 
     // Queue up jobs
-    function callback (err) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(util.format('Processed %s', url));
-        }
+    function callback (url) {
+        console.log(util.format('Processed %s', url));
     }
-    for (var z in range(minzoom, maxzoom + 1)) {
+    var levels = range(minzoom, maxzoom + 1);
+    for (var i=0; i < levels.length; i++) {
+        var z = levels[i];
         for (var x in range(0, Math.pow(2, z))) {
             for (var y in range(0, Math.pow(2, z))) {
                 var url = util.format('%s/%s/%s', z, x, y);
                 var zxy = [z, x, y];
-                console.log("Adding the following to the queue: ", zxy);
+                console.log("Adding the following to the queue:", url);
                 q.push({z:z, x:x, y:y}, callback);
+            }
+        }
+    }
+
+    // Task
+    function tileUploaderTask(task, callback) {
+        var url = util.format('%s/%s/%s', task.z, task.x, task.y);
+        console.log('Procesessing ' + url);
+
+        source.getTile(task.z, task.x, task.y, handleTile);
+
+        function handleTile(err, tile, headers) {
+            if (err) {
+                console.log(url, ' failed!');
+            } else {
+                // Save to tmpfile
+                var fname = url + '.pbf';
+                // var fdir = os.tmpdir();
+                var fdir = '/tmp';
+                var fpath = path.join(fdir, fname);
+
+                mkdirp(path.dirname(fpath), writeFile);
+                function writeFile(err) {
+                    if (err) throw (err);
+                    fs.writeFile(fpath, tile, uploadAndRmFile);
+                }
+                function uploadAndRmFile(err) {
+                    if (err) throw (err);
+                    cmd = 'swift upload ' + bucket + ' -H \'Content-Encoding: gzip\' -H \'Content-Type: application/x-protobuf\' -H \'X-Container-Meta-Access-Control-Allow-Origin: *\' ' + fname;
+                    runCmd(cmd, {cwd: fdir});
+                    fs.unlink(fpath);
+                }
+
+                callback(url);
             }
         }
     }
 });
 
+
+// Helpers
 
 /**
  * Generates an array containing arithmetic progressions
@@ -92,4 +106,14 @@ function range(start, stop) {
             return i + start;
         }
     );
+}
+
+function runCmd(cmd, opts) {
+    exec(cmd, opts, function(error, stdout, stderr) {
+        // console.log('stdout: ', stdout);
+        // console.log('stderr: ', stderr);
+        // if (error !== null) {
+            // console.log('exec error: ', error);
+        // }
+    });
 }
